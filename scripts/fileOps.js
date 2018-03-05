fa3df73ba8ffdb2f79823534e8556a642bd6d7d2
payaljain400 <jain.payal400@gmail.com>
2018-03-05 16:29:15 +0530

    Updated Backend Script

---
+++ scripts/fileOps.js
@@ -0,0 +1,114 @@
+var fs=require("fs");
+var natural=require("natural");
+var path=require("path");
+
+var tokenizer = new natural.WordTokenizer();
+
+var token,index=0,spellMist;
+var length=[],noun=[],verb=[],adverb=[],dictionary=[];
+
+let calcParametre=function(url){
+	return new Promise(function(resolve,reject){
+		var data=fs.readFileSync(url,"utf8");		//Read file using node fs
+		token=tokenizer.tokenize(data);				//Tokenize file data
+		length[index]=token.length;					//Calculating word length
+		var nn=0,vb=0,adv=0;
+		//Starting POS Tagger Area
+		var base_folder = path.join(path.dirname(require.resolve("natural")), "brill_pos_tagger");
+		var rulesFilename = base_folder + "/data/English/tr_from_posjs.txt";
+		var lexiconFilename = base_folder + "/data/English/lexicon_from_posjs.json";
+		var defaultCategory = 'N';		
+		var lexicon = new natural.Lexicon(lexiconFilename, defaultCategory);
+		var rules = new natural.RuleSet(rulesFilename);
+		var tagger = new natural.BrillPOSTagger(lexicon, rules);
+		var pos=tagger.tag(token);
+		for(i=0;i<pos.length;i++)
+		{
+			let word=pos[i];
+			//Checking For Nouns
+			if(word[1]=="NN" || word[1]=="NNS" || word[1]=="NNP" || word[1]=="NNPS")		
+				nn++;
+			//Checking for Verbs
+			else if(word[1]=="VB" || word[1]=="VBD" || word[1]=="VBG" || word[1]=="VBN" || word[1]=="VBP" || word[1]=="VBZ")
+				vb++;
+			//Checking for Adverbs
+			else if(word[1]=="RB" || word[1]=="RBR" || word[1]=="RBS")
+				adv+=1;
+		}
+		noun[index]=nn;
+		verb[index]=vb;
+		adverb[index]=adv;
+		console.log("Nouns: "+noun[index]);
+		resolve();
+	});
+};
+var dictionaryToken = function(url,file){
+	return new Promise(function(resolve,reject){
+		var data=fs.readFileSync(url,"utf8");
+		var dict=tokenizer.tokenize(data);		//Tokenized Dictionary for spell checking
+		var fdata=fs.readFileSync(file,"utf8");
+		var arr=tokenizer.tokenize(fdata);
+		dictionary=dict.concat(arr);
+		resolve();
+	});
+};
+var spellCheck = function(){
+	console.log("Inside Spellcheck");
+	var wrong=0;
+	var spellcheck = new natural.Spellcheck(dictionary);
+	token.map(function(word){
+		if(!spellcheck.isCorrect(word))
+		{
+			wrong += 1;
+			console.log(word);
+		}
+	});
+	spellMist = (wrong/token.length)*100;
+	console.log(wrong);
+};
+var objectCreate = function(){
+	obj={
+			output : {
+			perfect_length : length[0],
+			provided_length : length[1],
+			length_ratio : length[1]/length[0]*100,
+			perfect_nouns : noun[0],
+			provided_nouns : noun[1],
+			nouns_ratio : noun[1]/noun[0]*100,
+			perfect_verbs : verb[0],
+			provided_verbs : verb[1],
+			verb_ratio : verb[1]/verb[0]*100,
+			perfect_adverbs : adverb[0],
+			provided_adverbs : adverb[1],
+			adverb_ratio : adverb[1]/adverb[0]*100,
+			spelling_mistakes : spellMist
+		}
+	}
+	let json = JSON.stringify(obj,null,2);
+	console.log(json);
+	fs.writeFile("object.json",json,"utf8",(err)=>{
+		if(err)
+			console.log(err);
+		else
+		{
+			console.log("Success in writing JSON");
+		}
+
+	})
+};
+
+
+calcParametre("file1.txt").then(function(){		//Function Call for reading Perfect Document
+	index++;									//Index is 0 for perfect document and 1 for our document
+	return calcParametre("file2.txt");			//Reading the document to be tested
+}).then(function(){
+	index++;
+	return dictionaryToken("../dictionary.txt","file1.txt");
+}).then(function(){
+	console.log("Calling Spellcheck");
+	return spellCheck();
+}).then(function(){
+	return objectCreate();
+}).catch(function(error){
+	console.log(error);
+})
--- scripts/main.js
+++ scripts/main.js
@@ -10,37 +10,66 @@ function getData()
 	    {
 	       var myArr = JSON.parse(this.responseText);
 	       let res = 1 , rem = " ";
-	     //  var dataObj = JSON.stringify(myArr,null,2);
-	     //  document.getElementById("heading").innerHTML = dataObj;
-	     /*var len = myArr.output.perfect_length;
-	     console.log(len);*/
+
+	       /*DOM elements which displays the result on the index page*/
+
 	       document.getElementById("perlength").innerHTML = "Perfect Document's Length: "+myArr.output.perfect_length;
 	       document.getElementById("ylength").innerHTML = "Your Document's Length: "+myArr.output.provided_length;
 	       document.getElementById("lenper").innerHTML = "Length Percentage: "+myArr.output.length_ratio+"%";
-	       document.getElementById("perkey").innerHTML = "Perfect Document's Keyword: "+myArr.output.perfect_keywords;
-	       document.getElementById("ykey").innerHTML = "Your Document's Keyword: "+myArr.output.provided_keywords;
-	       document.getElementById("keyper").innerHTML = "Keyword Count Percentage: "+myArr.output.nouns_ratio+"%";
+	       document.getElementById("perkey").innerHTML = "Perfect Document's Noun: "+myArr.output.perfect_nouns;
+	       document.getElementById("ykey").innerHTML = "Your Document's Noun: "+myArr.output.provided_nouns;
+	       document.getElementById("keyper").innerHTML = "Noun Count Percentage: "+myArr.output.nouns_ratio+"%";
+	       document.getElementById("perverb").innerHTML = "Perfect Document's Verb Count: "+myArr.output.perfect_verbs;
+	       document.getElementById("yverb").innerHTML = "Your Document's Verb Count: "+myArr.output.provided_verbs;
+	       document.getElementById("verbper").innerHTML = "Verb Count Percentage: "+myArr.output.verb_ratio+"%";
+	       document.getElementById("peradverb").innerHTML = "Perfect Document's Adverb Count: "+myArr.output.perfect_adverbs;
+	       document.getElementById("yadverb").innerHTML = "Your Document's Adverb Count: "+myArr.output.provided_adverbs;
+	       document.getElementById("adverbper").innerHTML = "Adverb Count Percentage: "+myArr.output.adverb_ratio+"%";
 	       document.getElementById("spell").innerHTML = "Spelling Mistakes: "+myArr.output.spelling_mistakes+"%";
+	       
+	       /*Generating result on the basis of parametres generated*/
+
 	       if(myArr.output.provided_length < 1500 || myArr.output.spelling_mistakes>30 || myArr.output.nouns_ratio < 50)
 	       	res=0;
 	       if(res == 1)
 	       	document.getElementById("result").innerHTML = "Result: Document Accepted";
 	       else
 	       	document.getElementById("result").innerHTML = "Result: Document Rejected";
+
+	       /*Generating the remarks on the basis of the threshold values decided*/
+
 	       if(myArr.output.provided_length < 2500 || myArr.output.provided_length > 3500)
 	       {
 	       	res=2;
-	       	rem = "Word Length shoul be optimised. "
+	       	rem = "<br>Word Length shoul be optimised. "
+	       }
+	       if(myArr.output.nouns_ratio < 50)
+	       {
+	       	res=2;
+	       	rem += "<br>Keywords count is not optimum. "
+	       }
+	       if(myArr.output.verb_ratio < 50)
+	       {
+	       	res=2;
+	       	rem += "<br>Verb count is not optimum. "
+	       }
+	       if(myArr.output.adverb_ratio < 50)
+	       {
+	       	res=2;
+	       	rem += "<br>Adverb count is not optimum. "
 	       }	       	
 	       if(myArr.output.spelling_mistakes>15)
 	       {
 	       	res=2;
-	       	rem += "Spelling mistakes are prominent"
+	       	rem += "<br>Spelling mistakes are prominent"
 	       }
+
+	       /*Displaying Remarks on DOM*/
+
 	       if(res!=2)
 	       	document.getElementById("remarks").innerHTML = "Remarks: Good Documentation.Fulfills all the criterias.Job Well done.";
 	       else
-	       	document.getElementById("remarks").innerHTML = "Remarks: "+rem+".Kindly improve.";
+	       	document.getElementById("remarks").innerHTML = "Remarks:<br> "+rem+"<br>Kindly improve.";
 	    }
 	};
 }
